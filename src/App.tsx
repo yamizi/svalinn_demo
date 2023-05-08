d0ed95f90df4974ce383b8a628f620d890015ab4
Salah GHAMIZI <salah.ghamizi@gmail.com>
2023-05-08 10:34:17 +0200

    Fixed "<p>" javascript bug

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ function TabPanel(props: TabPanelProps) {
     >
       {value === index && (
         <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
+          <Box>{children}</Box>
         </Box>
       )}
     </div>
